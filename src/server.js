1aee13a546a2c4a4bed26a229f923bd73e5b2f06
Jacob Bare <jacob@limit0.io>
2018-04-10 12:30:26 -0500

    Add express file serving

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
 const { PORT } = process.env
 
-app.get('/file', (req, res) => {
-  res.send('This is the server!');
+app.get('/file/*', (req, res, next) => {
+  const options = {
+    root: path.resolve(__dirname, 'html'),
+    dotfiles: 'deny',
+  };
+  const file = req.params[0];
+
+  res.sendFile(file, options, (err) => {
+    if (err) {
+      console.error(err);
+      next(err);
+    } else {
+      console.info('Sent:', file);
+    }
+  });
 });
 
 app.listen(PORT, () => {
